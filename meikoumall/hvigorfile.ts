3b0cbf719ed5edafc4a9260deec697d9e484b73e
HeRping-rose <482832803@qq.com>
2025-08-07 17:53:32 +0800

    搜索页面功能实现 数据渲染 请求 加载数据 刷新 排序

--- meikoumall/hvigorfile.ts
+++ meikoumall/hvigorfile.ts
@@ -1,6 +1,8 @@
 import { appTasks } from '@ohos/hvigor-ohos-plugin';
-
+import { appPlugin } from "@hadss/hmrouter-plugin";/*使用路由编译插件 插件配置*/
 export default {
   system: appTasks, /* Built-in plugin of Hvigor. It cannot be modified. */
-  plugins: []       /* Custom plugin to extend the functionality of Hvigor. */
+  plugins: [
+    // appPlugin({ ignoreModuleNames: [/** 不需要扫描的模块 **/] })
+  ]       /* Custom plugin to extend the functionality of Hvigor. */
 }
--- meikoumall/products/phone/hvigorfile.ts
+++ meikoumall/products/phone/hvigorfile.ts
@@ -1,6 +1,11 @@
 import { hapTasks } from '@ohos/hvigor-ohos-plugin';
+import { hapPlugin } from "@hadss/hmrouter-plugin";
+
 
 export default {
   system: hapTasks, /* Built-in plugin of Hvigor. It cannot be modified. */
-  plugins: []       /* Custom plugin to extend the functionality of Hvigor. */
+  plugins: [
+    //配置插件 希望在当前包中使用插件
+    hapPlugin()
+  ]       /* Custom plugin to extend the functionality of Hvigor. */
 }
